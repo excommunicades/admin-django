efcab520ba312b792fa8f0352d0e73156c59e755
Daniil <daniilworkmail49@gmail.com>
2024-11-08 21:25:52 +0200

    Bug fixed

--- django-admin/django-admin/asgi.py
+++ django-admin/django-admin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoToDo project.
+ASGI config for django-admin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoToDo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-admin.settings')
 
 application = get_asgi_application()
--- django-admin/django-admin/urls.py
+++ django-admin/django-admin/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoToDo project.
+URL configuration for django-admin project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
